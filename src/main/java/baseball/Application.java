c924034fb7f8072dc4dfaea698fa181a2922bee0
ashehe <ash96235@gmail.com>
2023-09-25 18:08:43 +0900

    feat: 입력한 수에 대한 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,33 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.Objects;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    public static void main(String[] args) {
+        String answer = generateRandomNumber();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+        String guess = readLine();
+
+        boolean threeDigits = isGuessThreeDigits(guess);
+        boolean oneToNine = areAllDigitsOneToNine(guess);
+        boolean AllDistinct = areAllDigitsDistinct(guess);
+        boolean isGuessValid = isGuessValid(threeDigits, oneToNine, AllDistinct);
+
+        try {
+            String hint = printHint(isGuessValid, answer, guess);
+            System.out.println(hint);
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 입력입니다.");
+        }
+    }
+
+
     public static String generateRandomNumber() { // 랜덤한 숫자 생성
-        StringBuilder randomNumber = new StringBuilder("");
+        StringBuilder randomNumber = new StringBuilder();
         randomNumber.append(pickNumberInRange(1, 9));
         randomNumber.append(pickNumberInRange(1, 9));
         randomNumber.append(pickNumberInRange(1, 9));
@@ -18,11 +35,8 @@ public class Application {
         return answer;
     }
 
-    public static boolean isGuessThreeDigits(String guess) { // 입력이 세 자리수인지 검사
-        boolean threeDigits = false;
-        if (guess.length() == 3) {
-            threeDigits = true;
-        }
+    public static boolean isGuessThreeDigits(String guess) { // 입력이 세 자리 수인지 검사
+        boolean threeDigits = (guess.length() == 3);
         return threeDigits;
     }
 
@@ -51,17 +65,31 @@ public class Application {
     }
 
     public static boolean isGuessValid(boolean threeDigits, boolean oneToNine, boolean AllDistinct) { // 세 조건을 모두 만족하는 입력인지 검사
-        boolean isGuessValid = threeDigits && oneToNine && AllDistinct;
-        if (!isGuessValid) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
-        }
-        return isGuessValid;
+        return threeDigits && oneToNine && AllDistinct;
     }
 
-    public static void main(String[] args) {
-        generateRandomNumber();
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
-        String guess = readLine();
+    public static String printHint(boolean isGuessValid, String answer, String guess) { // 입력에 따라 힌트 또는 에러메시지 출력
+        String hint = "";
+        if (isGuessValid) {
+            hint = resultOfGuess(answer, guess);
+        } else {
+            throw new IllegalArgumentException();
+        }
+        return hint;
+    }
+
+    public static String resultOfGuess(String answer, String guess) { // 스트라이크, 볼 개수 계산
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int index = guess.indexOf(answer.charAt(i));
+            if (answer.charAt(i) == guess.charAt(i)) {
+                strike += 1;
+            } else if (index != -1) {
+                ball += 1;
+            }
+        }
+        return strike + "스트라이크 " + ball + "볼";
     }
 }
